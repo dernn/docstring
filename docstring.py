f92d48cf4c6c6e644d19c981e6c5e10bccc706de
dernn <fajj@mail.ru>
2024-03-14 14:28:04 +0600

    refactoring

--- docstring.py
+++ docstring.py
@@ -1,24 +1,49 @@
 # ХОРОШИЙ КОД ДОКУМЕНТИРУЕТ САМ СЕБЯ
 # или "хороший код будет понятен и без любой документации"
 
-# Пример «спагетти-кода», в котором одна функция выполняет несколько задач
-async def process_messages(messages, do_async):
-    for message in messages:
+# «Могу ли я сделать код читабельнее без документации?»
+
+import functools
+
+
+# Рефакторинг кода
+# Созданные атомарные функции можно будет легко переиспользовать в других местах,
+# уменьшая общее количество написанного кода.
+def catch_db_exceptions(wrapped):
+    @functools.wraps(wrapped)
+    def wrapper(*args, **kwargs):
         try:
-            user_id = message['user']['id']
-            user = db_conn.get('user', 'id=={}'.format(user_id))
-            user.messages = user.messages + '\n' + message['text']
-            user.save()
-            if do_async:
-                await db_conn.write_async(
-                    'message',
-                    {'text': message['text'], 'user_id': message['user']['id']}
-                )
-            else:
-                db_conn.write(
-                    'message',
-                    {'text': message['text'], 'user_id': message['user']['id']}
-                )
-            return 200, 'OK'
+            return wrapped(*args, **kwargs)
         except DatabaseException as exc:
             return 400, str(exc)
+
+    return wrapper
+
+
+def get_user(user_id):
+    return db_conn.get('user', 'id=={}'.format(user_id))
+
+
+def update_user_messages(user, message):
+    user.messages = user.messages + '\n' + message['text']
+    user.save()
+
+
+async def process_message(message, do_async):
+    data = {
+        'text': message['text'],
+        'user_id': message['user']['id']
+    }
+    if do_async:
+        await db_conn.write_async('message', data)
+    else:
+        db_conn.write('message', data)
+
+
+@catch_db_exceptions
+async def process_messages(messages, do_async):
+    for message in messages:
+        user = get_user(message['user']['id'])
+        update_user_messages(user, message)
+        await process_message(message, do_async)
+        return 200, 'OK'
